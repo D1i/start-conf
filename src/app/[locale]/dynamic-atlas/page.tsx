5d9660880b3a1aedb0d7817dde1b6b4988cb0b3a
K1ngVlad <kingvlad3008@gmail.com>
2025-07-14 20:27:01 +0400

    sadasdasdsa

--- src/app/[locale]/dynamic-atlas/page.tsx
+++ src/app/[locale]/dynamic-atlas/page.tsx
@@ -10,7 +10,7 @@ import { organsStorage } from '@/storage/organs-storage';
 import { selectedOrgan } from '@/storage/utilitar-storage';
 
 import style from './style.module.scss';
-// import { AuthorizedRouteContainer } from '@/components/HOC';
+import { AuthorizedRouteContainer } from '@/components/HOC';
 
 export default function DynamicAtlas() {
   const organsData = useAtomValue(organsStorage);
@@ -24,42 +24,42 @@ export default function DynamicAtlas() {
   }, []);
 
   return (
-    // <AuthorizedRouteContainer>
-    <div className={style.container}>
-      <div>
-        Left section
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
-          it's babylon container
-          <div className={style.organsContainer}>
-            {organsData.map((organ) => (
-              <Button
-                key={organ.id + 'selector'}
-                onClick={() => setCurrentSelectedOrgan(organ.id)}
-              >
-                {organ.name}
-              </Button>
-            ))}
+    <AuthorizedRouteContainer>
+      <div className={style.container}>
+        <div>
+          Left section
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            it's babylon container
+            <div className={style.organsContainer}>
+              {organsData.map((organ) => (
+                <Button
+                  key={organ.id + 'selector'}
+                  onClick={() => setCurrentSelectedOrgan(organ.id)}
+                >
+                  {organ.name}
+                </Button>
+              ))}
+            </div>
+            <div>Block with absolute position for switching layers</div>
           </div>
-          <div>Block with absolute position for switching layers</div>
-        </div>
-        <div
-          className={classname(style.infoContainer, {
-            [`${style.hidden}`]: isOpenInfoBlock,
-          })}
-        >
-          <div>
-            {organsData.find((organ) => organ.id === currentSelectedOrgan)
-              ?.description ?? 'organ not selected'}
-          </div>
-          <Button
-            onClick={handleToggleIsOpen}
-            className={style.openCloseToggler}
+          <div
+            className={classname(style.infoContainer, {
+              [`${style.hidden}`]: isOpenInfoBlock,
+            })}
           >
-            OPEN/CLOSE
-          </Button>
+            <div>
+              {organsData.find((organ) => organ.id === currentSelectedOrgan)
+                ?.description ?? 'organ not selected'}
+            </div>
+            <Button
+              onClick={handleToggleIsOpen}
+              className={style.openCloseToggler}
+            >
+              OPEN/CLOSE
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
-    // </AuthorizedRouteContainer>
+    </AuthorizedRouteContainer>
   );
 }
